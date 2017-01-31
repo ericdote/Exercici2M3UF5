4a15f28b217ce541b4b54a42dbff7c190a835c5a
Eric <Eric@192.168.120.81>
2017-01-31 16:13:58 +0100

    InfoClase casi acabada

--- src/exercici2/InfoClasse.java
+++ src/exercici2/InfoClasse.java
@@ -1,11 +1,66 @@
 package exercici2;
 
-public class InfoClasse <T>{
-    private T t1; 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import javax.lang.model.element.Element;
 
-    public InfoClasse(T t1) {
+public class InfoClasse<T> {
+
+    private Class<T> t1;
+
+    public InfoClasse(Class<T> t1) {
         this.t1 = t1;
+    }   
+
+    public InfoClasse() {
+    }
+
+    public String getNomSuperClass() {
+        return t1.getClass().getSuperclass().getName();
+    }
+
+    public String getNomClassComplet() {
+        return t1.getClass().toString();
+    }
+
+    public String getOnlyNomClass() {
+        return t1.getClass().getSimpleName();
+    }
+
+    public Field[] getArrayAttr() {
+        return t1.getClass().getDeclaredFields();
+    }
+
+    public Method[] getArrayMet() {
+        return t1.getClass().getDeclaredMethods();
+    }
+
+    public String[] getArrayNomAttr() {
+        String[] nomAtributs = new String[getArrayAttr().length];
+        Field[] att = getArrayAttr();
+        for (int i = 0; i < nomAtributs.length; i++) {
+            nomAtributs[i] = att[i].getName();
+        }
+        return nomAtributs;
+    }
+
+    public String[] getArrayNomMet() {
+        String[] nomMetodes = new String[getArrayMet().length];
+        Method[] att = getArrayMet();
+        for (int i = 0; i < nomMetodes.length; i++) {
+            nomMetodes[i] = att[i].getName();
+        }
+        return nomMetodes;
+    }
+
+    public T getInstanciaGenerica() throws InstantiationException, IllegalAccessException {
+        return (T) t1.getClass().newInstance();
     }
     
-    
+    @Override
+    public T clone() throws CloneNotSupportedException{
+        //TODO
+        return null;
+    }
+
 }
