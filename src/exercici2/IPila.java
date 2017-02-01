1bacd13cf259281434d9537e61d43f25f5465dc1
Eric <Eric@192.168.120.81>
2017-02-01 20:06:58 +0100

    IPila hecha

--- src/exercici2/IPila.java
+++ src/exercici2/IPila.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercici2;
 
-/**
- *
- * @author Eric
- */
-public class IPila {
+public interface IPila<T> {
+  
+    public void afegir();
+    
+    public T treure();
+    
+    public void buidar();
+    
+    public T mida();
+    
+    public T[] toArray();
     
 }
